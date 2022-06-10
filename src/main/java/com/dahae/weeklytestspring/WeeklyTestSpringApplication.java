e358bae014ab36709d87c55e0d952891784f8136
dahye <>
2022-06-10 17:33:36 +0900

    initial commit

---
+++ src/main/java/com/dahae/weeklytestspring/WeeklyTestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dahae.weeklytestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyTestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyTestSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dahae/weeklytestspring/WeeklyTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahae.weeklytestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyTestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
